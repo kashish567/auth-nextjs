af329042e8458055423c00c95af62dea1bf78893
kashish567 <kashishbhanushali7@gmail.com>
2024-06-22 19:04:38 +0530

    signup ui

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 
-const LoginPage = () => {
+const SignupPage = () => {
   return (
     <div className="flex justify-center items-center bg-blue-200 min-h-screen">
       <div className="bg-white p-10 shadow-lg rounded-lg">
-        <h1 className="font-bold text-blue-700">LOGIN</h1>
+        <h1 className="font-bold text-blue-700">SIGNUP</h1>
         <div className="flex flex-col my-4"></div>
+        <div className="flex flex-col my-4">
+          <label>Username</label>
+          <input
+            type="text"
+            className="border-2 outline-none  border-zinc-500 rounded-md px-2 py-1"
+          />
+        </div>
         <div className="flex flex-col my-4">
           <label>Email</label>
           <input
@@ -21,11 +28,11 @@ const LoginPage = () => {
           />
         </div>
         <button className="bg-blue-500 w-full py-1 my-2 rounded-md text-white shadow-sm hover:bg-blue-700">
-          Login
+          SignUp
         </button>
       </div>
     </div>
   );
 };
 
-export default LoginPage;
+export default SignupPage;
