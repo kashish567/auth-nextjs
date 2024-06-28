efe70edf50be1278ff2509bb99108b12d505b40f
kashish567 <kashishbhanushali7@gmail.com>
2024-06-28 14:53:59 +0530

    feat: logout route ang function made

---
+++ app/api/users/logout/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const res = NextResponse.json(
+      {
+        message: "Logout successful",
+        success: true,
+      },
+      { status: 200 }
+    );
+    res.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
+    return res;
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,30 @@
-import Image from "next/image";
+"use client";
+import axios from "axios";
+import { useRouter } from "next/navigation"; // Change this import
+import toast from "react-hot-toast";
 
 export default function Home() {
+  const router = useRouter();
+
+  const logoutHandler = async () => {
+    try {
+      const res = await axios.get("/api/users/logout");
+      router.push("/login");
+      toast.success(res.data.message);
+    } catch (error: any) {
+      toast.error(error.response.data.message);
+    }
+  };
+
   return (
-    <div className="bg-black flex justify-center text-pink-400">Hello</div>
+    <div>
+      <h1>I'm home</h1>
+      <button
+        onClick={logoutHandler}
+        className="bg-zinc-700 text-white px-2 py-1 rounded-md"
+      >
+        Logout
+      </button>
+    </div>
   );
 }
